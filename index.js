ecfe246cc48d9856b887515e1ff1362144ac68ea
Rafael Klock <kklockk@gmail.com>
2023-04-03 20:32:27 -0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send(
-    '<b>Desafio Levva</b> <br/><br/> RLK 2023 🍷! '
+    '<b>Levva</b> <br/><br/> RLK 2023! '
     ))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
